de9e59b3e6405eb8c32ead4fc15ebc6263cff032
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-03-12 16:53:11 -0700

    Initialize

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React, { SFC } from "react";
+import ReactDOM from "react-dom";
+
+const App: SFC = () => {
+  return <div />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
